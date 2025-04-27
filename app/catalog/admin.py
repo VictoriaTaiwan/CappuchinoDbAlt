8b5519c50391d2e3bf2f609a0522ba98a51476a2
VictoriaTaiwan <77235839+VictoriaTaiwan@users.noreply.github.com>
2025-04-27 23:30:04 +0800

    Added routes for products and basic HTML templates

--- app/catalog/admin.py
+++ app/catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, Recipe
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'calories', 'unit_name', 'image_src')
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'calories', 'unit_name', 'image_src', 'instructions', 'ingredients')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Recipe, RecipeAdmin)
