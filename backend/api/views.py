630c9826178201872daba9845aa70110fb030f77
VictoriaTaiwan <77235839+VictoriaTaiwan@users.noreply.github.com>
2025-04-29 16:40:15 +0800

    Fix view for the home page.

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,17 @@ from .models import Product, Recipe
 from .serializers import ProductSerializer, RecipeSerializer
 
 from rest_framework import viewsets
+from django.shortcuts import render
+
+
+def index(request):
+    num_products = Product.objects.all().count()
+    num_recipes = Recipe.objects.count()
+    context = {
+    'num_products': num_products,
+    'num_recipes': num_recipes,
+    }
+    return render(request, 'index.html', context=context)
 
 class ProductViewSet(viewsets.ModelViewSet):
 	queryset = Product.objects.all()
