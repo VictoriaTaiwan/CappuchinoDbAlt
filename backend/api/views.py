e10faee03fb805a11575ef8037d4bbaee46b248f
VictoriaTaiwan <77235839+VictoriaTaiwan@users.noreply.github.com>
2025-05-01 16:31:17 +0800

    Remove static files from the backend and allow views to display json formatted data in the browser.

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,22 +2,13 @@ from .models import Product, Recipe
 from .serializers import ProductSerializer, RecipeSerializer
 
 from rest_framework import viewsets
-from django.shortcuts import render
-
-
-def index(request):
-    num_products = Product.objects.all().count()
-    num_recipes = Recipe.objects.count()
-    context = {
-    'num_products': num_products,
-    'num_recipes': num_recipes,
-    }
-    return render(request, 'index.html', context=context)
+from rest_framework.renderers import JSONRenderer
 
 class ProductViewSet(viewsets.ModelViewSet):
 	queryset = Product.objects.all()
 	serializer_class = ProductSerializer 
-
+	renderer_classes = [JSONRenderer]
 class RecipeViewSet(viewsets.ModelViewSet):
 	queryset = Recipe.objects.all()
 	serializer_class = RecipeSerializer  
+	renderer_classes = [JSONRenderer]
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,12 +1,8 @@
-from django.contrib import admin
+
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.decorators.cache import never_cache
-from api import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('api/', include('api.urls')),
-    path('', never_cache(views.index))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
